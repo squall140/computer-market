29450445902ab5f7f97b6758a977a67b26cf0912
Leif <squall140@gmail.com>
2025-01-07 14:59:42 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Welcome to My React App</h1>
+      </main>
+    </div>
+  );
+}
+
+
+
+export default App;
